a849ced5cb8d0f10ab1db5cf2e03dea3b6ad6891
MOZZ\admin <yeong0319@naver.com>
2023-02-09 00:48:49 +0900

    feat: 몽고DB URL변경

--- schemas/index.js
+++ schemas/index.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery',true)
 
+require('dotenv').config();
+
 const connect = () => {
-  mongoose
-  .connect("mongodb://127.0.0.1:27017/sm_CRUD01")
-  .catch(err => console.log(err));
+    mongoose
+    .connect(process.env.mongoDb)
+    .catch(err => console.log(err));
 };
 
 mongoose.connection.on("error", err => {
-  console.error("몽고디비 연결 에러", err);
+    console.error("몽고디비 연결 에러", err);
 });
 
-module.exports = connect;
+module.exports = connect
