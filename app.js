e2d9ceb0905aef19b48e4417dd773094253690d4
MOZZ\admin <yeong0319@naver.com>
2023-02-13 16:49:02 +0900

    feat:회원가입 로그인 기능구현

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 3000;
 
 const postRouter = require('./routes/posts.js');
-const commentRouter = require("./routes/comments.js")
+const commentRouter = require("./routes/comments.js");
+const signupRouter = require("./routes/user.js");
+const loginRouter = require("./routes/login.js")
 
 const connect = require("./schemas/index.js");
 
-app.use(express.json());
+
 connect();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-
+app.use("/signup",[signupRouter]);
+app.use("/login",[loginRouter]);
 app.use("/posts", [postRouter]);
 app.use("/comments", [commentRouter])
 
---
+++ routes/login.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../schemas/user.js");
+const jwt = require("jsonwebtoken");
+
+router.post("/", async(req,res) => {
+    const {nickname, password} = req.body;
+    const check = await User.findOne({nickname});
+
+    if(!check || check.password !== password){
+        res.status(412).json({errorMassege : "닉네임 또는 패스워드를 확인해주세요"})
+    }
+
+    const token = jwt.sign({userId: check.userId},"customized-secret-key");
+
+    res.cookie("Authorization", `Bearer ${token}`);
+    res.status(200).json({token : token})
+});
+
+
+module.exports = router;
